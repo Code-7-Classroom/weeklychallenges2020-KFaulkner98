b6503c70150a9fbb563c11259504be5eea7e26ca
Kyle Antonio <KAntonio@macbook-pro.home>
2020-03-30 11:07:22 -0400

    Array Challenges

---
+++ Checkpoint-Arrays/checkpoint.js
@@ -0,0 +1,11 @@
+// 1. Name Three JS array methods
+// .push(adds an element to the end)
+// .pop(removes the last element)
+// .shift(removes the first element)
+
+// 2. Code
+var drinks = ['water', 'coffee', 'tea'];
+var food = ['apple', 'sandwich', 'chips'];
+
+var meals= drinks.concat(food);
+console.log(meals);
