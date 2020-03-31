54fd5a8151961a2e8161b8257fe358a650f13804
Kyle Antonio <KAntonio@macbook-pro.home>
2020-03-30 22:23:18 -0400

    e-Commerce Project

---
+++ e-Commerce/JS/index.js
@@ -0,0 +1,43 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+// Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nxtBtn = document.querySelector('#nxtBtn');
+
+// Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// Button Listeners
+
+nxtBtn.addEventListener('click', () => {    // When you click the Next button it will move to the next slide
+    if (counter >= carouselImages.length -1) return;
+    carouselSlide.style.transition = "transform 1.3s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+});
+
+prevBtn.addEventListener('click', () => { // Previous button will go back a slide
+    if (counter <= 0) return;
+    carouselSlide.style.transition = "transform 1.3s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+});
+
+carouselSlide.addEventListener('transitionend', () => {
+    if (carouselImages[counter].id === 'lastClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }    
+    if (carouselImages[counter].id === 'firstClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }    
+});
